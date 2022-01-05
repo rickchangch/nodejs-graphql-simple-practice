538af16ec21f63623a1995f7595060887d5c3158
Rick Chang <rick.zhang@atelli.ai>
2022-01-06 00:26:42 +0800

    feat: implement REST API with express.js

---
+++ web_application/index.js
@@ -0,0 +1,83 @@
+/**
+ * require pkg
+ */
+var express = require('express');
+var bodyParser = require('body-parser');
+
+/**
+ * global variable
+ */
+const PORT = 3000;
+var app = express();
+
+/**
+ * setup middleware
+ */
+// for parsing application/json
+app.use(bodyParser.json());
+// for parsing application/x-www-form-urlencoded, extended=true allow input nested obejct (ex. {a: {b: 0}})
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// hardcode user info
+var userList = [
+  {
+    name: 'Rick',
+    age: 87,
+  },
+];
+
+// get
+app.get('/users/:id', function(req, resp) {
+
+  // show query string
+  console.log(req.query);
+  // show path params
+  console.log(req.params);
+
+  resp.status(200).send(userList[req.params.id] || {});
+});
+
+// list
+app.get('/users', function(req, resp) {
+  resp.status(200).send(userList);
+});
+
+// create
+app.post('/users', function(req, resp) {
+
+  // get body params
+  let bodyParams = req.body;
+
+  // append to userList
+  userList.push(bodyParams);
+
+  resp.sendStatus(201);
+});
+
+// update
+app.put('/users/:id', function(req, resp) {
+
+  // get resource ID
+  let id = req.params.id;
+  // get body params
+  let bodyParams = req.body;
+
+  // updated
+  userList[id] = bodyParams;
+
+  resp.sendStatus(200);
+});
+
+// delete
+app.delete('/users/:id', function(req, resp) {
+
+  // remove from userList
+  userList.splice(req.params.id, 1);
+
+  resp.sendStatus(204);
+});
+
+// activate server
+app.listen(PORT, function() {
+  console.log('The server is activating now and listening on port %d!', PORT);
+});
